9c026e142ef7113b021d962c948c1a2d45f52f9b
Tyler Newman <tylerjnewman18@gmail.com>
2015-08-07 15:22:00 -0700

    add index template for board

--- app/assets/javascripts/views/boards_index.js
+++ app/assets/javascripts/views/boards_index.js
@@ -1,4 +1,26 @@
 TrelloClone.Views.BoardsIndex = Backbone.View.extend({
-  // template:
-  // render:
+  template: JST['boards/index'],
+
+    initialize: function () {
+      this.listenTo(
+        this.collection,
+        'add remove change:title',
+        this.render
+      );
+    },
+
+    render: function () {
+      var renderedContent = this.template();
+      this.$el.html(renderedContent);
+
+      this.collection.each(function (board) {
+        var indexItem = new TrelloClone.Views.BoardsIndexItem({
+          model: board
+        });
+        debugger;
+        this.$('ul').append(indexItem.render().$el);
+      }.bind(this));
+
+      return this;
+    }
 });
